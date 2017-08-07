dd9cc781d9d4de2a107534fcd0f856083772664c
KunnyChen <abc582700457@qq.com>
2017-08-07 16:57:37 +0800

    blog update

--- blog/js/index.js
+++ blog/js/index.js
@@ -2,37 +2,16 @@
     $(".menu-phone").click(function() {
         $(this).addClass("active");
         if ($(this).hasClass("active")) {
-            $("#access").show();
+            $(".phone-access").show();
+          document.body.style.overflow='hidden';
         } else {
-            $("#access").hide();
+            $(".phone-access").hide();
+            document.body.style.overflow='scroll';
         }
     });
-    $("#access").click(function() {
+    $(".phone-access").click(function() {
         $(".menu-phone").removeClass("active");
-        $("#access").hide();
+        $(".phone-access").hide();
+         document.body.style.overflow='scroll';
     });
 })()
-
-// (function() {
-//     $(".menu-phone").click(function() {
-//         var hide = $("#access").hasClass("hide");
-//          $(this).toggleClass('active');
-//         function hideMenu() {
-//             // $("#access").addClass("hide");
-//             setTimeout(function() {
-//                 $("#access").addClass("hide");
-//             }, 100);
-//         }
-//         if (hide)
-//             $("#access").removeClass("hide");
-//         else {
-//             hideMenu();
-//             $(".menu-phone").removeClass('active');
-//         }
-//         $("#access").click(function() {
-//             $("#access").addClass("hide");
-//              $(".menu-phone").removeClass('active');
-//         })
-
-//     })
-// })()
---
+++ templatesy_574/574/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/Izumenko/pen/BWoWXQ.
+
+ Original shot https://dribbble.com/shots/3323188-Experimental-Hamburger-Menue
